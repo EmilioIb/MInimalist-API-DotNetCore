fb7436a267ace7125f2f0ca091245d595d35a8d2
IntegraCorp <IntegraCorp@INTEGRACORP>
2023-10-12 09:31:28 -0600

    Agregar archivos de proyecto.

---
+++ ApiPruebas/Models/PIzza.cs
@@ -0,0 +1,9 @@
+﻿ namespace ApiPruebas.Models
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsGlutenFree { get; set; }
+    }
+}
